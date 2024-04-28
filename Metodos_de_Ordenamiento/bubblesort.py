0cc075cd3683f415d3153cf68d801d6aa371853a
Mendez Emiliano <emilome03@gmail.com>
2024-04-28 15:56:44 -0600

    bubblesort con los comentarios

--- Metodos_de_Ordenamiento/bubblesort.py
+++ Metodos_de_Ordenamiento/bubblesort.py
@@ -1,27 +1,26 @@
-#bubble sort 3/4/2024
 
-#def is how you define a function follow by the name and the type of data yor are gonna pass
+listaDesordenada = [1,8,5,2,0]
+print(listaDesordenada)
+
+#necesitamos ir una vez por cada elemento en la lista
+
 def bsort(arreglo):
-    n = len(arreglo)
-
-    for i in range(0,n-2):
-        for j in range(0,n-1):
+    for i in range(len(arreglo)): 
+        #i representa cada elemento de la lista ya arreglada
+        #al principio son 0 elementos ordenados, luego 1,2,3.....len que seria el total de elementos en la lista
+        #compararar cada elemento que no esta ordenado y hacerle swap si es necesario
+        for j in range(len(arreglo)-i-1): 
+            #restandole i le dicimios que elementos faltan para arreglar y restandole el 1 es para no considerar el ultimo elemento
             if arreglo[j] > arreglo[j+1]:
+                #si los elementos no estan en orden haz lo siguiente
                 temp = arreglo[j]
                 arreglo[j] = arreglo[j+1]
-                arreglo[j+1] = temp
+                arreglo[j+1] = temp           
     return arreglo
     
 
-listaDesordenada = [8,7,1,4,3,2,1]
-
-print(listaDesordenada)
-
 listaOrdenada = bsort(listaDesordenada)
-
 print(listaOrdenada)
 
-print("Ya quedo el bubblesort")
 
-print("holaaa")
-
+print("Ya quedo el bubblesort") 
