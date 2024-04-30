39b103f2be95c7d4f23b85588943ce1e0b2ae763
unmetro <emlome22@gmail.com>
2024-04-30 15:20:30 -0600

    quicksort

--- Metodos_de_Ordenamiento/quicksort.py
+++ Metodos_de_Ordenamiento/quicksort.py
@@ -1,39 +1,16 @@
-
 import random
 
-def quicksort(arr, izq, der): #tiene el arreglo, luego tiene el principio y el final del arreglo
-    if izq < der: 
-        posparticion = particion(arr, izq, der)
-        quicksort(arr, izq, posparticion - 1) #llamamos quicksort a todos los elemetnos que son menor a nuestro pivot
-        quicksort(arr, posparticion + 1, der)#llamamos a quicksort a todos los elementos que son mayor a nuestro pivot
+def quicksort(arreglo): #recibe un arreglo
+    if len(arreglo) > 1: #si arreglo es mayor a 1 se ejecuta el codigo 
+        pivot = arreglo[len(arreglo)//2] #establecemos nuestro pivote que es el elemento en la mitad
+        arr_izq = [i for i in arreglo if i < pivot] #este arreglo almacena todos los elementos menores al pivote
+        arr_mid = [i for i in arreglo if i == pivot] #este todos los elementos iguales al pivote
+        arr_der = [i for i in arreglo if i > pivot] #este todos los elemetnos mayores al pivote
+        return quicksort(arr_izq) + arr_mid + quicksort(arr_der) #es lo recursivo para que se llame a si misma
     return arreglo
-    
 
-def particion(arr, izq, der): #aqui declaramos nuestro pivot
-    i = izq - 1 #iniciamos i que sea en -1 del inicio del arreglo
-    pivot = arr[der] #nuestro pivot va a ser el ultimo elemento del arreglo
+lista = [i for i in range(0,15,2)]
+random.shuffle(lista)
+print(lista)
 
-    for j in range(izq, der): #j va a ir por todo el arreglo
-        #si el elemnto actual es menor al pivote
-        if arr[j] < pivot:
-            i += 1 #incrimenetamos i 
-            temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-            #intecambio de las posiciones del elemento i y j en el arreglo
-        print(arreglo)
-    
-    #intercambio del pivote con el elemnto en la posicion i + 1      
-    temp = arr[i + 1]
-    arr[i + 1] = arr[der]
-    arr[der] = temp
-    return i + 1
-
-arreglo = [x*3 for x in range(-1,10,2)]
-random.shuffle(arreglo)
-print(arreglo)
-
-quicksort(arreglo, 0, len(arreglo)-1)
-print(arreglo)
-            
-            
+print(quicksort(lista))
